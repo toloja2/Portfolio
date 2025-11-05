86ab238b0c2e3fad241072eccc2481e6330b1842
Tolojanahary Ranarijaonina <tolojaranarijaonina@gmail.com>
2025-11-05 22:34:47 +0300

    style avec js

--- main.js
+++ main.js
@@ -1,59 +1,74 @@
-
-function homeSection() {
-  const homeSection = document.getElementById("homeSection");
-  const aboutSection = document.getElementById("aboutSection");
-  const seeCvSection = document.getElementById("seeCvSection");
-  const contactSection = document.getElementById("contactSection");
-  homeSection.style.display = "flex";
-  aboutSection.style.display = "none";
-  seeCvSection.style.display = "none";
-  contactSection.style.display = "none";
-  location.reload();
+// SCROLL VERS AUTRE CHOSES
+function vers(id, type) {
+    const element = document.getElementById(id)
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: type
+        })
+    }
 }
 
-function aboutSection() {
-  const homeSection = document.getElementById("homeSection");
-  const aboutSection = document.getElementById("aboutSection");
-  const seeCvSection = document.getElementById("seeCvSection");
-  const contactSection = document.getElementById("contactSection");
-  homeSection.style.display = "none";
-  aboutSection.style.display = "flex";
-  seeCvSection.style.display = "none";
-  contactSection.style.display = "none";
+// VOIR MENU CACHE
+const bouttonMenu = document.getElementById('bouttonMenu');
+const navList = document.querySelector('nav>ul');
+bouttonMenu.addEventListener("click",()=>{
+    navList.classList.toggle('offancaveMenu');
+    bouttonMenu.firstChild.classList.toggle("fa-close");
+    document.addEventListener("click", (e) => {
+        if (!bouttonMenu.contains(e.target)) {
+            navList.classList.remove("offancaveMenu");
+            bouttonMenu.firstChild.classList.replace("fa-close", "fa-bars")
+        }
+})})
+
+// Anymation
+const ajoutAnimation = document.getElementById('ajoutAnimation');
+const styleKeyframes = document.createElement("style")
+ajoutAnimation.addEventListener('click',()=>{
+        styleKeyframes.textContent = `@keyframes animation1 {
+    0% {
+        width: 0;
+        margin-left: -5%;
+    }
+
+    100% {
+        width: 85%;
+    }
 }
 
-function seeCvSection() {
-  const homeSection = document.getElementById("homeSection");
-  const aboutSection = document.getElementById("aboutSection");
-  const seeCvSection = document.getElementById("seeCvSection");
-  const contactSection = document.getElementById("contactSection");
-  homeSection.style.display = "none";
-  aboutSection.style.display = "none";
-  seeCvSection.style.display = "flex";
-  contactSection.style.display = "none";
+@keyframes animation2 {
+    0% {
+        width: 0;
+        margin-left: -5%;
+    }
+
+    100% {
+        width: 65%;
+    }
 }
 
-function contactSection() {
-  const homeSection = document.getElementById("homeSection");
-  const aboutSection = document.getElementById("aboutSection");
-  const seeCvSection = document.getElementById("seeCvSection");
-  const contactSection = document.getElementById("contactSection");
-  const formulaire = document.getElementById("formulaire");
-  homeSection.style.display = "none";
-  aboutSection.style.display = "none";
-  seeCvSection.style.display = "none";
-  contactSection.style.display = "flex";
-  formulaire.reset();
-}
+@keyframes animation3 {
+    0% {
+        width: 0;
+        margin-left: -5%;
+    }
 
-function envoyeMessage(){
-  const sucessMessage = document.getElementById("sucessMessage");
-  const formulaire = document.getElementById("formulaire");
-  sucessMessage.style.display = "flex";
-  formulaire.style.display = "none";
- 
-}
+    100% {
+        width: 60%;
+    }
+}`
+    document.body.appendChild(styleKeyframes)
+    document.getElementById('aboutSection').addEventListener("mouseleave",()=>{
+         styleKeyframes.textContent = '';
+    })
+})
 
-function reloadPage(){
-  location.reload();
-}
+// VOIR PLUS A PROPOS DE MOI
+const voirPlus = document.getElementById("voirPlus");
+const progressBar2 = document.getElementById("progressBar2");
+
+voirPlus.addEventListener("click",()=>{
+  voirPlus.textContent = voirPlus.textContent === 'Voir Plus' ? 'Voir Moins' : 'Voir Plus' ;
+   progressBar2.style.display = progressBar2.style.display === 'none' ? 'block' : 'none';
+})
